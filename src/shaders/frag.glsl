4cfbacfdd5a0a29aff1910cba316ab14efd31f9f
YacineMK <merroukiyacine06@example.com>
2024-08-10 03:05:14 +0200

    last project

--- src/shaders/frag.glsl
+++ src/shaders/frag.glsl
@@ -6,9 +6,9 @@ void main() {
     float y = pos.y/3.0;
     float x = pos.x/3.0;
 
-    if( x < -0.08 && x > -0.3 &&  y/x < 0.7){
+    if(y<0.01 && x < -0.12 && x > -0.3 &&  y/x < 0.55){
         gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0);
-    } else if( -y<0.009 &&  x < -0.08 && x > -0.3 &&  -y/x > 0.65){
+    } else if(-y<0.01 && x < -0.12 && x > -0.3 &&  y/x > -0.55){
         gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0);
     }else if(y > black){
         gl_FragColor = vec4(0.0, 0.0, 0.0, 1.0);
